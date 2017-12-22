9596295761cff476f28e6a08b9e430d0aaa5f8f8
dabai2017 <1904812793@qq.com>
2017-12-22 15:44:29 +0800

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,16 @@
+class 乘法表
+{
+	public static void main(String[] args){
+
+		for(int a=1;a<=9;a++){
+
+			for(int b=1;b<=a;b++){
+				System.out.print(a+"*"+b+"="+(a*b)+"\t");
+
+			}
+			System.out.println("\n");
+		}
+
+	}
+
+}
